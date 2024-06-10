ad2e6e455ba3ad008c43114187c5dbe3ed90e227
annafricks <annafricks33@gmail.com>
2024-06-10 17:01:41 -0400

    day 1 - routes added

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const cors = require("cors")
 const PORT = 5005;
+
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
-
+const cohorts= require("./cohorts.json")
+const students= require("./students.json")
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
@@ -24,9 +27,17 @@ app.use(cookieParser());
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
-// ...
+
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
+});
+
+app.get("/api/cohorts", (req, res)=>{
+  res.json(cohorts)
+})
+
+app.get("/api/students", (req, res)=>{
+  res.json( students );
 });
 
 
